fde0921d6641e187d3699cb10277a2e492d417ff
NikSavenkov <niksavenkov132@gmail.com>
2019-03-17 11:27:54 +1000

    New vk bot

---
+++ WebApplication1/CallbackController.cs
@@ -0,0 +1,107 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using VkNet.Abstractions;
+using VkNet.Model;
+using VkNet.Model.RequestParams;
+using VkNet.Utils;
+using VkNet;
+using VkNet.Enums.Filters;
+using VkNet.Enums.SafetyEnums;
+using System;
+
+namespace Cookie.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+
+
+
+
+
+    public class CallbackController : ControllerBase
+    {
+
+        static string[] Commands = { "hello" };
+
+        /// <summary>
+        /// Конфигурация приложения
+        /// </summary>
+        private readonly IConfiguration _configuration;
+
+        private readonly IVkApi _vkApi;
+
+        public CallbackController(IVkApi vkApi, IConfiguration configuration)
+        {
+            _configuration = configuration;
+
+            _vkApi = vkApi;
+
+
+
+
+
+            
+
+        }
+
+        
+
+
+
+        [HttpPost]
+        public IActionResult Callback([FromBody] Updates updates)
+        {
+            // Проверяем, что находится в поле "type" 
+            switch (updates.Type)
+            {
+                
+
+
+                // Если это уведомление для подтверждения адреса
+                case "confirmation":
+                    // Отправляем строку для подтверждения 
+                    return Ok(_configuration["Config:Confirmation"]);
+
+                case "message_new":
+                    {
+                        // Десериализация
+                        var msg = Message.FromJson(new VkResponse(Updates.Object));
+
+                        long? userID = msg.UserId;
+
+                        // Отправим в ответ полученный от пользователя текст
+                        void SendMessage (string message)
+                        {
+
+                            _vkApi.Messages.Send(new MessagesSendParams
+                            {
+
+
+                                RandomId = new DateTime().Millisecond,
+                                PeerId = msg.PeerId.Value,
+                                Message = message,
+                                UserId = userID
+
+                            });
+                        }
+
+                        SendMessage("Привет");
+
+                        break;
+                    }
+
+                    
+
+            }
+            // Возвращаем "ok" серверу Callback API
+            return Ok("ok");
+
+            
+        }
+
+
+
+
+
+    }
+}
---
+++ WebApplication1/Pattern.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace Cookie.Controllers
+{
+    [Serializable]
+    public class Updates
+    {
+        public static JToken Object { get; internal set; }
+
+        /// <summary>
+        /// Тип события
+        /// </summary>
+        [JsonProperty("type")]
+        public string Type { get; set; }
+
+        /// <summary>
+        /// Объект, инициировавший событие
+        /// Структура объекта зависит от типа уведомления
+        /// </summary>
+        [JsonProperty("object")]
+        public Object JObject { get; set; }
+
+        /// <summary>
+        /// ID сообщества, в котором произошло событие
+        /// </summary>
+        [JsonProperty("group_id")]
+        public long GroupId { get; set; }
+    }
+}
